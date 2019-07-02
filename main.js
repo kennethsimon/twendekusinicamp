1cc00d4b035fac42808baf62a931dff0ee732316
kennethsimon <kenethsimon@gmail.com>
2019-07-02 14:18:03 +0300

    d

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.owl-carousel').owlCarousel({
+      items:1,
+      margin:0,
+      autoplay: true,
+      loop: true,
+      autoHeight:true,
+  });
+
+});
